7f39821389e427517750f081769985b8ff1b8a23
Sebastian Hungerecker <sepp2k@googlemail.com>
2020-05-14 23:14:58 +0200

    Initial version with syntax highlighting and sabas64 integration

---
+++ README.md
@@ -0,0 +1,23 @@
+# commodore-basic-vscode README
+
+This is an extension providing syntax highlighting for Commodore BASIC V2 (the version of BASIC used in the C64).
+
+## Features
+
+* Syntax highlighting
+* Error reporting using the sabas64 static analyzer
+* Planned: auto-completion
+
+TODO: Screenshot
+
+## Requirements
+
+sabas64 needs to be available in the `PATH`
+
+## Known Issues
+
+Other versions of BASIC are not supported. Neither are custom syntax extensions.
+
+## Release Notes
+
+Not yet released
---
+++ src/extension.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+import {spawn} from 'child_process';
+
+export function activate(context: vscode.ExtensionContext): void {
+    const diagnosticCollection = vscode.languages.createDiagnosticCollection();
+    if (vscode.window.activeTextEditor) {
+		analyze(vscode.window.activeTextEditor.document, diagnosticCollection);
+	}
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
+		analyze(event.document, diagnosticCollection);
+	}));
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		if (editor) {
+			analyze(editor.document, diagnosticCollection);
+		}
+	}));
+}
+
+function analyze(document: vscode.TextDocument, collection: vscode.DiagnosticCollection) {
+    if (document) {
+		const sabas = spawn("sabas64", ["--json"]);
+		let stdout = "";
+		sabas.stdout.on('data', data => stdout += data);
+		sabas.on('close', (code) => {
+			const issues = JSON.parse(stdout) as Array<any>;
+			collection.set(document.uri, issues.map(issue => {
+				const line = issue.location.actualLine;
+				const sc = issue.location.startColumn;
+				const ec = issue.location.endColumn;
+				const severity = issue.priority === "ERROR" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning;
+				return new vscode.Diagnostic(
+					new vscode.Range(new vscode.Position(line - 1, sc), new vscode.Position(line - 1, ec)),
+					issue.message,
+					severity
+				);
+			}));
+		});
+		sabas.stdin.write(document.getText());
+		sabas.stdin.destroy();
+	} else {
+		collection.clear();
+	}
+}
